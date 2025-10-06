856b572201d6a6073b72f95982d7a1b6ce0de277
Oscar David huerta <odhuerta@cps.edu>
2025-10-06 10:37:47 -0500

    finished common cards

---
+++ cards.py
@@ -0,0 +1,74 @@
+from typing import List, Tuple
+
+movie_db: List[Tuple[str, int, List[str]]] = [
+    (
+        "common", #rarity
+        1, #elixir count
+        [
+            "ice spirit" #card troops
+            "skeletons"
+            "electro spirit"
+            "fire spirit"
+        ]
+    ),
+    (
+        "common",
+        2,
+        [
+            "bomber"
+            "spear goblins"
+            "bats"
+            "berserker"
+            "zap"
+            "giant snowball"
+        ]
+    ),
+    (
+        "common",
+        3,
+        [
+            "knight"
+            "archers"
+            "minions"
+            "goblin gang"
+            "skeleton barrel"
+            "fire cracker"
+            "cannon"
+            "arrows"
+            "royal delivery"
+        ]
+    ),
+    (
+        "common",
+        4,
+        [
+            "skeleton dragons"
+            "mortar"
+            "tesla"
+        ]
+    ),
+    (
+        "common",
+        5,
+        [
+            "minion hoarde"
+            "rascals"
+            "barbarians"
+        ]
+    ),
+    (
+        "common",
+        6,
+        [
+            "royal giant"
+            "elite barbarians"
+        ]
+    ),
+    (
+        "common",
+        7,
+        [
+            "royal recruits"
+        ]
+    )
+]
