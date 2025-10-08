67a0a08715cdd15c73c8951ce29c44415b8b5b00
Oscar David huerta <odhuerta@cps.edu>
2025-10-08 10:43:16 -0500

    made some functions

--- a3.py
+++ a3.py
@@ -16,3 +16,60 @@ def get_cost(card: Tuple[str, str, int, List[str]]) -> int:
 
 def get_cards(card: Tuple[str, str, int, List[str]]) -> List[str]:
     return card[3]
+
+########################################################################
+
+def rarity_by_type(matches: List[str]) -> List[str]:
+    troop = matches[0]
+    result = []
+
+    for cards in cards_db:
+        if get_type(cards) == troop:
+            result.append(get_rarity(cards))
+    return result
+
+def rarity_by_cost(matches: List[str]) -> List[str]:
+    troop = int(matches[0])
+    result = []
+
+    for cards in cards_db:
+        if get_cost(cards) == troop:
+            result.append(get_rarity(cards))
+    return result
+
+def rarity_by_cards(matches: List[str]) -> List[str]:
+    troop = matches[0]
+    result = []
+
+    for cards in cards_db:
+        if troop in get_cards(cards):
+            result.append(get_rarity(cards))
+    return result
+
+def type_by_rarity(matches: List[str]) -> List[str]:
+    rarity = matches[0]
+    result = []
+
+    for cards in cards_db:
+        if get_rarity(cards) == rarity:
+            result.append(get_type(cards))
+    return result
+
+def type_by_cost(matches: List[str]) -> List[str]:
+    cost = int(matches[0])
+    result = []
+
+    for cards in cards_db:
+        if get_cost(cards) == cost:
+            result.append(get_type(cards))
+    return result
+
+def type_by_cards(matches: List[str]) -> List[str]:
+    card_name = matches[0]
+    result = []
+
+    for cards in cards_db:
+        if card_name in get_cards(cards):
+            result.append(get_type(cards))
+    return result
+
